94b57cdecf840fb2adf1973faaa2f1ed8e33aa66
jjpimentaoliveira <developer.pimenta@gmail.com>
2023-12-22 11:18:26 +0000

    Implement pokemon details + shiny swap

--- Dex3/Dex3/PokemonDetail.swift
+++ Dex3/Dex3/PokemonDetail.swift
@@ -10,9 +10,58 @@ import SwiftUI
 
 struct PokemonDetail: View {
     @EnvironmentObject var pokemon: Pokemon
+    @State var showShiny = false
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ZStack {
+                Image(.normalgrasselectricpoisonfairy)
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: .black, radius: 6)
+
+                AsyncImage(url: showShiny ? pokemon.shiny : pokemon.sprite) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.top, 50)
+                        .shadow(color: .black, radius: 6)
+
+                } placeholder: {
+                    ProgressView()
+                }
+            }
+
+
+            HStack {
+                ForEach(pokemon.types!, id: \.self) { type in
+                    Text(type.capitalized)
+                        .font(.title2)
+                        .shadow(color: .white, radius: 1)
+                        .padding([.top, .bottom], 8)
+                        .padding([.leading, .trailing])
+                        .background(Color(type.capitalized))
+                        .clipShape(RoundedRectangle(cornerRadius: 50.0))
+                }
+
+                Spacer()
+            }
+            .padding()
+        }
+        .navigationTitle(pokemon.name!.capitalized)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    showShiny.toggle()
+                } label: {
+                    if showShiny {
+                        Image(systemName: "wand.and.stars").foregroundStyle(.yellow)
+                    } else {
+                        Image(systemName: "wand.and.starts.inverse")
+                    }
+                }
+            }
+        }
     }
 }
 
